1dda967ad3ed7589dc67dcbcbbae2580f1c2a3ec
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2015-05-12 10:07:01 +0200

    OwlCarousel CSS / JS Update

--- Resources/Public/Javascripts/Originals/markup.js
+++ Resources/Public/Javascripts/Originals/markup.js
@@ -1,52 +1,29 @@
-/**
- * Created by sebo on 04.03.15.
- */
-(function($) {
-	if ( $("#tx-ecom-product-tools-carousel").length ) {
-		var owl = $("#tx-ecom-product-tools-carousel");
-		owl.owlCarousel({
-			items: 4,
-			itemsDesktop : [1199,4],
-			itemsDesktopSmall : [980,2],
-			itemsTablet: [768,3],
-			itemsTabletSmall: false,
-			itemsMobile : [479,1],
-			navigation: true
-		});
+! function(o) {
+	var items = 4;
 
-		checkMarkupWidth(owl);
-/*
-		$('#approval-list .next').click(function(){
-			owl.trigger('owl.next');
-		});
-		$('#approval-list .prev').click(function(){
-			owl.trigger('owl.prev');
-		});
-*/
-
-		window.onresize = function() {
-			checkMarkupWidth(owl);
-		};
-
-		$('.item', owl).click(function() {
-			var rel = '#' + $(this).attr('rel');
-			$('html, body').animate({
-				scrollTop: $(rel).offset().top - 100
-			}, 500);
-		});
+	if (o("#tx-ecom-product-tools-carousel").length) {
+		var t = o("#tx-ecom-product-tools-carousel");
+		t.owlCarousel({
+			items: items,
+			dots: false,
+			nav: o("#tx-ecom-product-tools-carousel .item").length > items ? true : false,
+			responsive: {
+				0: {
+					items: 2
+				},
+				481: {
+					items: items
+				}
+			}
+		}), o(".item", t).click(function() {
+			var t = "#" + o(this).attr("rel");
+			o("html, body").animate({
+				scrollTop: o(t).offset().top - 100
+			}, 500)
+		})
 	}
-})(jQuery);
 
-
-function checkMarkupWidth(owl) {
-	var outerWrapW = $("#approval-list .approval-wrap").outerWidth();
-	var itemsW = 0;
-	owl.find(".item").each(function() {
-		itemsW = itemsW + $(this).outerWidth();
-	});
-	if( outerWrapW <= itemsW - 2 ) {
-		owl.removeClass("fit");
-	} else {
-		owl.addClass("fit");
+	if (o("#tx-ecom-product-tools-carousel .item").length <= items) {
+		o("#tx-ecom-product-tools-carousel").addClass('fit');
 	}
-}
+}(jQuery);
