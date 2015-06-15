0af28c139ba88d56ab91689f43c262ef38e12f98
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2015-06-15 12:02:25 +0200

    Owl Slider Update

--- Resources/Public/Javascripts/Originals/markup.js
+++ Resources/Public/Javascripts/Originals/markup.js
@@ -1,12 +1,14 @@
-! function(o) {
+! function($) {
 	var items = 4;
+	var prevButton = '.owl-prev';
+	var nextButton = '.owl-next';
 
-	if (o("#tx-ecom-product-tools-carousel").length) {
-		var t = o("#tx-ecom-product-tools-carousel");
-		t.owlCarousel({
+	if ($("#tx-ecom-product-tools-carousel").length) {
+		var owl = $("#tx-ecom-product-tools-carousel");
+		owl.owlCarousel({
 			items: items,
 			dots: false,
-			nav: o("#tx-ecom-product-tools-carousel .item").length > items ? true : false,
+			nav: $("#tx-ecom-product-tools-carousel .item").length > items ? true : false,
 			responsive: {
 				0: {
 					items: 2
@@ -15,15 +17,39 @@
 					items: items
 				}
 			}
-		}), o(".item", t).click(function() {
-			var t = "#" + o(this).attr("rel");
-			o("html, body").animate({
-				scrollTop: o(t).offset().top - 100
+		}), $(".item", owl).click(function() {
+			var item = "#" + $(this).attr("rel");
+			$("html, body").animate({
+				scrollTop: $(item).offset().top - 100
 			}, 500)
-		})
+		});
+		toggleNavigation();
+		owl.on('translated.owl.carousel', function () { toggleNavigation(); });
 	}
 
-	if (o("#tx-ecom-product-tools-carousel .item").length <= items) {
-		o("#tx-ecom-product-tools-carousel").addClass('fit');
+	if ($("#tx-ecom-product-tools-carousel .item").length <= items) {
+		$("#tx-ecom-product-tools-carousel").addClass('fit');
+	}
+
+	function toggleNavigation() {
+		// Only 1 Page
+		if(owl.find(".owl-item").last().hasClass('active') && owl.find(".owl-item").first().hasClass('active')) {
+			owl.find(nextButton).addClass("off");
+			owl.find(prevButton).addClass("off");
+		}
+		// Next
+		else if(owl.find(".owl-item").last().hasClass('active')){
+			owl.find(nextButton).addClass("off");
+			owl.find(prevButton).removeClass("off");
+		}
+		// Prev
+		else if(owl.find(".owl-item").first().hasClass('active')) {
+			owl.find(nextButton).removeClass("off");
+			owl.find(prevButton).addClass("off");
+		}
+		else {
+			owl.find(prevButton).removeClass("off");
+			owl.find(nextButton).removeClass("off");
+		}
 	}
 }(jQuery);
